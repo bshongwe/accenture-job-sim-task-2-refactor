290d01fe0d3a60df37e242fe239f42fdac70b4ce
bshongwe <dru.da.beat.hustle@gmail.com>
2024-04-16 14:45:07 +0200

    Injecting SearchService into SearchController

--- src/main/java/com/mockcompany/webapp/controller/SearchController.java
+++ src/main/java/com/mockcompany/webapp/controller/SearchController.java
@@ -14,8 +14,10 @@ package com.mockcompany.webapp.controller;
 /*
  * An import statement allows the current class to use the class being imported
  */
-import com.mockcompany.webapp.data.ProductItemRepository;
+//import com.mockcompany.webapp.data.ProductItemRepository;
 import com.mockcompany.webapp.model.ProductItem;
+/* to use SearchService */
+import com.mockcompany.webapp.services.SearchService;
 /* The springframework package allows us to take advantage of the spring capabilities */
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -54,10 +56,14 @@ public class SearchController {
      *   The SearchService will need to have the ProductItemRepository declared and injected instead.
      */
 
+    /* Refactored: compare with lines 63 - 66 */
     @Autowired
-    public SearchController(ProductItemRepository productItemRepository) {
-        this.productItemRepository = productItemRepository;
-    }
+    private SearchService searchService;
+
+//    @Autowired
+//    public SearchController(ProductItemRepository productItemRepository) {
+//        this.productItemRepository = productItemRepository;
+//    }
 
     /**
      * TODO: Refactor this class by creating a new class named SearchService in the
@@ -74,42 +80,46 @@ public class SearchController {
      */
     @GetMapping("/api/products/search")
     public Collection<ProductItem> search(@RequestParam("query") String query) {
-        Iterable<ProductItem> allItems = this.productItemRepository.findAll();
-        List<ProductItem> itemList = new ArrayList<>();
+        return searchService.search(query);
+    }
 
-        boolean exactMatch = false;
-        if (query.startsWith("\"") && query.endsWith("\"")) {
-            exactMatch = true;
+//    public Collection<ProductItem> search(@RequestParam("query") String query) {
+//        Iterable<ProductItem> allItems = this.productItemRepository.findAll();
+//        List<ProductItem> itemList = new ArrayList<>();
+
+//        boolean exactMatch = false;
+//        if (query.startsWith("\"") && query.endsWith("\"")) {
+//            exactMatch = true;
             // Extract the quotes
-            query = query.substring(1, query.length() - 1);
-        } else {
+//            query = query.substring(1, query.length() - 1);
+//        } else {
             // Handle case-insensitivity by converting to lowercase first
-            query = query.toLowerCase();
-        }
+//            query = query.toLowerCase();
+//        }
 
         // For each item... This is written for simplicity to be read/understood not necessarily maintain or extend
-        for (ProductItem item : allItems) {
-            boolean nameMatches;
-            boolean descMatches;
+//        for (ProductItem item : allItems) {
+//            boolean nameMatches;
+//            boolean descMatches;
             // Check if we are doing exact match or not
-            if (exactMatch) {
+//            if (exactMatch) {
                 // Check if name is an exact match
-                nameMatches = query.equals(item.getName());
+//                nameMatches = query.equals(item.getName());
                 // Check if description is an exact match
-                descMatches = query.equals(item.getDescription());
-            } else {
+//                descMatches = query.equals(item.getDescription());
+//            } else {
                 // We are doing a contains ignoring case check, normalize everything to lowercase
                 // Check if name contains query
-                nameMatches = item.getName().toLowerCase().contains(query);
-                // Check if description contains query
-                descMatches = item.getDescription().toLowerCase().contains(query);
-            }
+//                nameMatches = item.getName().toLowerCase().contains(query);
+//                // Check if description contains query
+//                descMatches = item.getDescription().toLowerCase().contains(query);
+//            }
 
             // If either one matches, add to our list
-            if (nameMatches || descMatches) {
-                itemList.add(item);
-            }
-        }
-        return itemList;
-    }
+//            if (nameMatches || descMatches) {
+//                itemList.add(item);
+//            }
+//        }
+//        return itemList;
+//    }
 }
